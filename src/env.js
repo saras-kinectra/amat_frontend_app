22d9a321a36c16c862d2de4e02316d55a5133e78
yogidigitaltaas <yogi@digitaltaas.com>
2019-03-30 04:08:40 +0530

    configured https ssl to make oauth work in chrome

--- src/env.js
+++ src/env.js
@@ -2,6 +2,6 @@
     window.__env = window.__env || {};
   
     // API url
-    window.__env.apiUrl = 'http://ec2-34-229-95-172.compute-1.amazonaws.com/amatg3mapper/api/';
+    window.__env.apiUrl = 'https://ec2-34-229-95-172.compute-1.amazonaws.com/amatg3mapper/api/';
   
   }(this));
