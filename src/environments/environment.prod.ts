a8df3afb28837344f58c90b1f6262dcb2ae1a21d
yogidigitaltaas <yogi@digitaltaas.com>
2019-04-04 13:26:00 +0530

    environrment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ec2-34-229-95-172.compute-1.amazonaws.com/amatg3mapper/api/',
+  apiUrl: 'http://ec2-34-229-95-172.compute-1.amazonaws.com/amatg3mapper/api/',
   baseHref : '/amatg3mapper/app/',
   issuer_uri: 'https://myloginqa.amat.com:8080',
   client_id: 'G3Mobile_Dev',
-  redirect_uri: 'https://ec2-34-229-95-172.compute-1.amazonaws.com/amatg3mapper/app/callback',
+  redirect_uri: 'http://ec2-34-229-95-172.compute-1.amazonaws.com/amatg3mapper/app/callback',
   scope:'openid profile',
   extras: {
     'access_type': 'offline'
